8ade184da042ee1934beb385fba9912da1739088
Diego Nascimento <diego.nascimento@liferay.com>
2017-12-20 15:29:47 -0300

    Adding createResourceUrl to mock

--- src/__mocks__/DummyActionPortlet.js
+++ src/__mocks__/DummyActionPortlet.js
@@ -1,8 +1,22 @@
 import PortletImpl from '../impl';
+import Uri from 'metal-uri';
 
 class DummyActionPortlet extends PortletImpl {
   executeAction(p, e) {
     return new Promise((resolve, reject) => resolve([]));
+  }
+  createResourceUrl(param, cache, resid) {
+    let str = '';
+    if (typeof param == 'object') {
+      for (var key in object) {
+        if (key[value] != undefined) {
+          str += JSON.stringify(key);
+        }
+      }
+    } else {
+      str = JSON.stringify(param);
+    }
+    return new Uri(`http://dummyportlet/?${str}&portletId=${resid}&${cache}`);
   }
 }
 
